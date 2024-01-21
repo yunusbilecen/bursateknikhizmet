0848a4873b823ad88c22cd69c6366e2236fedc83
yunusbilecen <yunussbilecen@gmail.com>
2024-01-21 21:32:49 +0300

    server ready

--- next.config.js
+++ next.config.js
@@ -1,17 +1,4 @@
 // next.config.js
 module.exports = {
   reactStrictMode: false,
-  webpack: (config, { isServer }) => {
-    // isServer durumuna göre modülleri devre dışı bırakma
-    if (!isServer) {
-      config.resolve.fallback = {
-        fs: false,
-        dns: false,
-        net: false,
-        tls: false,
-      };
-    }
-
-    return config;
-  },
 };
