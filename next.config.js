a1adf605ce52bc9bdaf41404890ebdf7a05debcf
yunusbilecen <yunussbilecen@gmail.com>
2024-01-21 18:48:13 +0300

    change vercel.json

--- next.config.js
+++ next.config.js
@@ -8,15 +8,6 @@ module.exports = {
       config.plugins.push(new webpack.HotModuleReplacementPlugin());
     }
 
-    // fs modülünü client-side'da devre dışı bırakma
-    if (!isServer) {
-      config.resolve.fallback = {
-        fs: false,
-        dns: false,
-        net: false,
-        tls: false
-      };
-    }
 
     return config;
   },
