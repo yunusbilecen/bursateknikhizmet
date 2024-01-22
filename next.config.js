f8e528b60b01d3e3a58cc40fbbc2781a43e1ce61
yunusbilecen <yunussbilecen@gmail.com>
2024-01-22 19:48:30 +0300

    chnaged next

--- next.config.js
+++ next.config.js
@@ -1,3 +1,19 @@
 module.exports = {
   reactStrictMode: true,
+  // Diğer temel yapılandırmalar buraya eklenebilir
+
+  // Statik HTML çıktısı için export ayarı
+  // Bu özelliği ekleyerek statik HTML çıktısı alabilirsiniz
+  // Daha fazla ayrıntı için: https://nextjs.org/docs/advanced-features/static-html-export
+  exportPathMap: async function () {
+    return {
+      '/': { page: '/' },
+      // Diğer sayfalar buraya eklenebilir
+    };
+  },
+
+  // Statik HTML çıktısını out klasörüne al
+  // Bu, next export komutu kullanıldığında kullanılır
+  // Daha fazla ayrıntı için: https://nextjs.org/docs/advanced-features/static-html-export#output-directory
+  outDir: 'out',
 };
