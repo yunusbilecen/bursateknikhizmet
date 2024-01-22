011d34400a1d0fbc570d69213a22afb7490a03c2
yunusbilecen <yunussbilecen@gmail.com>
2024-01-22 19:01:57 +0300

    chnage next config

--- next.config.js
+++ next.config.js
@@ -1,31 +1,9 @@
 // next.config.js
 module.exports = {
-  reactStrictMode: true, // ya da false, ihtiyacınıza göre değiştirin
-  // "target" artık desteklenmiyor, kaldırılabilir veya belirli bir değere ayarlanabilir.
-  // Örneğin, eğer "serverless" kullanıyorsanız, onu "server" olarak değiştirebilirsiniz.
-  // Ancak, bu kısmı kaldırarak da deneyebilirsiniz, çünkü varsayılan değer "server" olarak ayarlıdır.
-  target: 'serverless',
+  reactStrictMode: true, // veya false, ihtiyacınıza göre değiştirin
+  targer: 'server',
   webpack: (config, { isServer }) => {
-    // Özel Webpack yapılandırmalarınızı burada ekleyebilirsiniz.
-
-    // Örnek: Image optimizer eklemek için
-    config.module.rules.push({
-      test: /\.(png|jpe?g|gif|svg)$/i,
-      use: [
-        {
-          loader: 'file-loader',
-          options: {
-            outputPath: 'static/images',
-            publicPath: '/_next/static/images',
-          },
-        },
-      ],
-    });
-
-    if (!isServer) {
-      // Client tarafındaki özel yapılandırmaları buraya ekleyebilirsiniz.
-    }
-
+    // Webpack özelleştirmeleri...
     return config;
   },
 };
